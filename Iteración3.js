b413525376c5de4a1b88345a0ecc608589dc85f8
ramiroisla <ramiro.isla@bootcamp-upgrade.com>
2022-04-27 17:01:39 +0200

    Iteration#3

---
+++ Iteración3.js
@@ -0,0 +1,22 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let num1 = 10;
+let num2 = 5;
+alert(num1*num2);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+let num3 = 2;
+alert(num1 / num3)
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+let y = 10;
+let z = 5;
+let x = y + z;
+console.log(x);
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+x = y * z;
+console.log(x);
